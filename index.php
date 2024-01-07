1456d18deea3c1f1d5a842321fa680ebd8591bee
Andrew <frintu.andrei07@gmail.com>
2024-01-07 21:06:53 +0200

    header section + background

--- index.php
+++ index.php
@@ -13,7 +13,45 @@
 </head>
 <body>
 
-    
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap');
+        :root {
+            /* CSS HEX */
+            --white: #fafafaff;
+            --forest-green-crayola: #4ea46fff;
+            --amazon: #3c7853ff;
+            --tropical-rain-forest: #326e5cff;
+            --black: #020202ff;
+        }
+        html {
+            height: 100vh;
+            overflow: hidden;
+        }
+        body {
+            background: url("images/fundal.jpg") !important;
+            background-size: cover !important;
+            scroll-behavior: smooth;
+    	    scroll-snap-type: y mandatory;
+            overflow-y: scroll;
+            height: 100vh;
+        }
+        .scroll-snap {
+            scroll-snap-stop: always;
+            scroll-snap-align: center;
+        }
+    </style>
+
+    <div id="top" class="bg-light scroll-snap doubleHeader text-center w-100 py-1 d-lg-flex justify-content-center align-items-center gap-lg-3 border-bottom">
+        <p class="ms-lg-1 w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#despre">Despre</a></p>
+        <p class="w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#indici-indicatori">Indici & Indicatori</a></p>
+        <p class="w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#cauze">Cauze</a></p>
+        <p class="w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#consecinte">Consecințe</a></p>
+        <p class="w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#combatere">Combatere</a></p>
+        <p class="me-lg-1 w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#contact">Contact</a></p>
+    </div>
+    <header class="bg-faded w-100 py-2 mb-4 border-bottom text-center shadow-lg d-lg-block d-flex">
+        <span class="text-white fs-4 fw-bold m-auto">INFLAȚIA</span>
+    </header>
 
 </body>
 </html>
