04b0acb948dfa2654a4ab01014349e8bced88f4e
Andrew <frintu.andrei07@gmail.com>
2024-01-07 21:11:33 +0200

    css file

--- index.php
+++ index.php
@@ -7,39 +7,12 @@
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <title>Inflația - Istorie și Societate în Dimensiunea Digitală - Andrei Frîntu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="ajax/styles.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="ajax/scripts.js"></script>
 </head>
 <body>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap');
-        :root {
-            /* CSS HEX */
-            --white: #fafafaff;
-            --forest-green-crayola: #4ea46fff;
-            --amazon: #3c7853ff;
-            --tropical-rain-forest: #326e5cff;
-            --black: #020202ff;
-        }
-        html {
-            height: 100vh;
-            overflow: hidden;
-        }
-        body {
-            background: url("images/fundal.jpg") !important;
-            background-size: cover !important;
-            scroll-behavior: smooth;
-    	    scroll-snap-type: y mandatory;
-            overflow-y: scroll;
-            height: 100vh;
-        }
-        .scroll-snap {
-            scroll-snap-stop: always;
-            scroll-snap-align: center;
-        }
-    </style>
 
     <div id="top" class="bg-light scroll-snap doubleHeader text-center w-100 py-1 d-lg-flex justify-content-center align-items-center gap-lg-3 border-bottom">
         <p class="ms-lg-1 w-75 m-auto fs-5 mb-0 fit"><a rel="nofollow" class="nav-link py-2 px-4 rounded" href="#despre">Despre</a></p>
